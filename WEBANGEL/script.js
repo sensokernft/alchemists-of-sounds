c47127ba2da5dc3c92821006f59fb702f6ff7b97
sensokernft <105549320+sensokernft@users.noreply.github.com>
2025-11-16 13:28:41 +0100

    Add files via upload
    Primera subida de Archivos

---
+++ WEBANGEL/script.js
@@ -0,0 +1,7 @@
+// Ejemplo de reserva simple
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Web cargada y lista.");
+
+    // Aquí podrías integrar Google Analytics
+    // gtag('config', 'TU_ID_DE_ANALYTICS');
+});
